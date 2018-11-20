325316bbfb6ddd1413e29d304bae6ef467bfb03a
Michael Park <mcypark@gmail.com>
2018-11-20 12:10:22 -0800

    Simplified `template/diff.py` and `s/replaced/rm/`.

--- template/diff.py
+++ template/diff.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """
-A Pandoc filter to create non-code diffs. `add` and `remove` are the classes
-that can be added to a `Div` or a `Span`.  `add` colors the text green, and
-`remove` colors the text red. For HTML, `add` also underlines the text, and
-`remove` also strikes out the text.
+A Pandoc filter to create non-code diffs. `add` and `rm` are the classes that
+can be added to a `Div` or a `Span`.  `add` colors the text green, and `rm`
+colors the text red. For HTML, `add` also underlines the text, and `rm` also
+strikes out the text.
 
 # Example
 
@@ -32,47 +32,39 @@ def action(elem, doc):
     if not isinstance(elem, pf.Div) and not isinstance(elem, pf.Span):
         return None
 
-    diff = None
+    color_name = None
+    tag_name = None
     for cls in elem.classes:
-        if cls == 'add' or cls == 'remove':
-            diff = cls
+        color_name = cls + 'color'
+        if cls == 'add':
+            tag_name = 'ins'
+        elif cls == 'rm':
+            tag_name = 'del'
 
-    if diff is None:
+    if tag_name is None:
         return None
 
-    color_name = diff + 'color'
-    color_begin = pf.RawInline('{{\\color{{{}}}'.format(color_name), 'tex')
-    color_end = pf.RawInline('}', 'tex')
+    open_tag = pf.RawInline('<{}>'.format(tag_name), 'html')
+    open_color = pf.RawInline('{{\\color{{{}}}'.format(color_name), 'tex')
+    close_color = pf.RawInline('}', 'tex')
+    close_tag = pf.RawInline('</{}>'.format(tag_name), 'html')
+
     color = doc.get_metadata(color_name)
     attributes = {} if color is None else {'style': 'color: #{}'.format(color)}
 
-    def format(*args):
-        if diff == 'add':
-            return pf.Span(pf.RawInline('<ins>'), *args, pf.RawInline('</ins>'))
-        elif diff == 'remove':
-            return pf.Span(pf.RawInline('<del>'), *args, pf.RawInline('</del>'))
-
     if isinstance(elem, pf.Div):
-        def visit(elem, doc):
-            if isinstance(elem, pf.Header):
-                return pf.Header(format(*elem.content))
-            elif isinstance(elem, pf.Para):
-                return pf.Para(format(*elem.content))
-            elif isinstance(elem, pf.Plain):
-                return pf.Plain(format(*elem.content))
-            elif isinstance(elem, pf.LineItem):
-                if elem.content:
-                    head, *tail = elem.content
-                    if isinstance(head, pf.Str) and head.text.isspace():
-                        return pf.LineItem(head, format(*tail))
-                return pf.LineItem(format(*elem.content))
-        elem.walk(visit)
-        return pf.Div(pf.Plain(color_begin),
-                      pf.Div(*elem.content),
-                      pf.Plain(color_end),
+        return pf.Div(pf.Plain(open_tag),
+                      pf.Plain(open_color),
+                      elem,
+                      pf.Plain(close_color),
+                      pf.Plain(close_tag),
                       attributes=attributes)
     elif isinstance(elem, pf.Span):
-        return pf.Span(color_begin, format(*elem.content), color_end,
+        return pf.Span(open_tag,
+                       open_color,
+                       elem,
+                       close_color,
+                       close_tag,
                        attributes=attributes)
 
 if __name__ == '__main__':
