72d2af83b51534147c17021b5c90cded7a8878c3
Michael Park <mcypark@gmail.com>
2022-02-14 14:43:52 -0800

    Add the Boost License for the framework portion of the repo.

--- data/toc-depth.py
+++ data/toc-depth.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# MPark.WG21
+#
+# Copyright Michael Park, 2022
+#
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
 import sys
 import yaml
 
